5406af231b06bab34d11b93bac080d55efc3ee13
Andrew Vasilchenko <cazonove@gmail.com>
2024-04-19 22:27:15 +0300

    ready 2 pages

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/header.js';
 import './components/burger.js';
 import './components/transfer.js';
+import './components/tabs.js';
---
+++ src/js/components/tabs.js
@@ -0,0 +1,5 @@
+import GraphTabs from 'graph-tabs';
+
+if (document.querySelector('[data-tabs="categoryTabs"]')) {
+	const categoryTabs = new GraphTabs('categoryTabs');
+}
